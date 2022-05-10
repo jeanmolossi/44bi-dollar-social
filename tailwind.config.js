a4820c3b972b848e7d396d905d722ec7cdb8c379
Jean <jean.carlosmolossi@gmail.com>
2022-05-09 22:12:53 -0300

    feat: novas cores de temas adicionadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     ],
     darkMode: false,
     theme: {
-        extend: {},
+        extend: {
+			colors: {
+				'twitter': {
+					100: '#f0f8ff',
+					200: '#c6e2ff',
+					300: '#7fdfff',
+					400: '#00c6ff',
+					500: '#00b5e3',
+					600: '#0099c6',
+					700: '#0080b1',
+					800: '#006699',
+					900: '#004d80',
+				},
+				'twitter-dark': {
+					100: '#eff3f4',
+					500: '#5b7083',
+					600: '#536471',
+				}
+			}
+		},
     },
     variants: {},
     plugins: [],
