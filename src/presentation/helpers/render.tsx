ef2fc8d3547ddfb611054a13aad92f8f1391d22f
Jean <jean.carlosmolossi@gmail.com>
2022-05-12 00:08:49 -0300

    feat: criado helper de renderizacao condicional

--- src/presentation/helpers/render.tsx
+++ src/presentation/helpers/render.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 /**
+ * RenderIf is a helper to render if a condition true
+ * @param condition Condition to be evaluated to show the component
+ * @param children The component to be rendered
+ * @returns The component to be rendered
+ */
+export function RenderIf(condition: boolean, children: React.ReactNode) {
+	return condition === true ? <>{children}</> : null;
+}
+
+/**
  * SwapComponent is a helper function to render one of two components based on a condition.
  * @param condition Condition to be evaluated to show the component
  * @param RenderWhenConditionTrue The children to be rendered when the condition is true
