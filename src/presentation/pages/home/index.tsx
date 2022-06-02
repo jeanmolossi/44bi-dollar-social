e4f765027fd9f2bfaca484ee176751df82f35004
Jean <jean.carlosmolossi@gmail.com>
2022-06-01 23:28:15 -0300

    feat: criada sidebar e menu

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -2,8 +2,22 @@ import React from "react";
 
 export function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
+		<div className="min-h-screen flex columns-2">
+			<header
+				role="banner"
+				className="flex flex-col items-end basis-auto grow max-w-[595px]"
+			>
+				<div className="relative w-[275px] flex flex-col items-stretch flex-shrink-0 basis-auto">
+					<div className="fixed top-0 h-full px-3 flex flex-col justify-between">
+						<div>Menu</div>
+						<div>Bottom</div>
+					</div>
+				</div>
+			</header>
+
+			<main role="main" className="items-start flex basis-auto grow">
+				Main content
+			</main>
 		</div>
 	)
 }
