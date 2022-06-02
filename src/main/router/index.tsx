1c4777d8ada7eeb292af12678bb46ad7e25fa520
Jean <jean.carlosmolossi@gmail.com>
2022-06-01 22:23:49 -0300

    feat: usando componente de spinner como fallback

--- src/main/router/index.tsx
+++ src/main/router/index.tsx
@@ -1,3 +1,4 @@
+import { LoadingSpinner } from "@/presentation/components/loading-spinner";
 import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -10,7 +11,7 @@ export function Router() {
 				<Route
 					index
 					element={
-						<Suspense fallback={"Loading page..."}>
+						<Suspense fallback={<LoadingSpinner size="large" />}>
 							<MakeLoginFactory />
 						</Suspense>
 					}
